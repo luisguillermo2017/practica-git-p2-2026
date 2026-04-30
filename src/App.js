d46c521e7a6f7ffe3060cdeeead622e45a97a385
avilac26 <emanuelavilac@gmail.com>
2026-04-29 20:58:47 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hola Mundo Desde develop_emanuel
         </a>
       </header>
     </div>
